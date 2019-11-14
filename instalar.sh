b618924021efe04a4e76495cd2d4b7d3424be930
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-14 12:15:36 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -141,8 +141,7 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/MaxiiARG/IFS-ADM/master/Install/trans &> /dev/null
 msg -bar2
-msg -bra ""
-msg -mag "\e[30mINSTALANDO PANEL ADMINISTRATIVO\e[0m"
+msg -bra "INSTALANDO PANEL ADMINISTRATIVO"
 msg -bra " [ IFS - ADM | 2019 ] "
 msg -bar2
 [[ $1 = "" ]] && funcao_idioma || {
