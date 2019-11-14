0ea8a8a95a852ab9e0fdb215c8f7938da2bb0fa1
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-14 12:35:04 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -145,6 +145,7 @@ msg -bar2
 msg -bra "INSTALANDO PANEL ADMINISTRATIVO"
 msg -bra " [ IFS - ADM | 2019 ] "
 msg -bra "SELECCIONE UN IDIOMA:"
+msg -xd
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
