3f46d19c8cbc0bf1a11071ffc6b0a42057bb644b
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-14 13:33:22 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -141,7 +141,7 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/MaxiiARG/IFS-ADM/master/Install/trans &> /dev/null
 msg -bar2
-msg -ama "[ NEW - ULTIMATE - SCRIPT ]"
+msg -ama "[ \e[5mIFS ADM ]"
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
