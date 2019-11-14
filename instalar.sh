736fd06a745a37cb4150fc161d81fd5369e84aa9
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-14 12:51:54 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -142,8 +142,8 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/MaxiiARG/IFS-ADM/master/Install/trans &> /dev/null
 msg -bar2
-msg -bra " PANEL IFS "
-msg -bra " SELECCIONE UN IDIOMA: "
+msg -bra "\e[1;37mPANEL IFS\e[0m"
+msg -bra "\e[1;37mSELECCIONE UN IDIOMA:\e[0m"
 msg -bar
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
