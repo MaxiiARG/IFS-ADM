8733077e2b8eac10dac8efa1038b99b57d2e316a
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2019-11-15 19:50:25 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -6,8 +6,6 @@ SCPidioma="${SCPdir}/idioma"
 SCPusr="${SCPdir}/ger-user"
 SCPfrm="/etc/ger-frm"
 SCPinst="/etc/ger-inst"
-SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL01heGlpQVJHL0lGUy1BRE0vbWFzdGVyL3JlcXVlc3Q="
-SUB_DOM='base64 -d'
 [[ $(dpkg --get-selections|grep -w "gawk"|head -1) ]] || apt-get install gawk -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "mlocate"|head -1) ]] || apt-get install mlocate -y &>/dev/null
 rm $(pwd)/$0 &> /dev/null
