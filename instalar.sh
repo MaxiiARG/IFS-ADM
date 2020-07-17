665283db2b11d8c817d7114e4c789f193304e434
MaxiiARG <40850209+MaxiiARG@users.noreply.github.com>
2020-07-16 21:04:14 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -161,7 +161,7 @@ REQUEST=$(echo $SCPresq|$SUB_DOM)
 IP="104.238.135.147" && echo "$IP" > /usr/bin/vendor_code
 cd $HOME
 msg -ne "Key: "
-wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verified" || {
+wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verificado" || {
    echo -e "\033[1;32m Verified"
    invalid_key
    exit
